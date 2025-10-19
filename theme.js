61e812932ef1f4c0e106557de543ebfb94366e2a
Muhammed Halil <mhalil@caltech.edu>
2025-10-19 22:28:08 +0300

    u

--- theme.js
+++ theme.js
@@ -5,7 +5,7 @@ function DarkMode() {
 
     if (dark) {
         document.body.style.backgroundImage = "linear-gradient(to right, black, black ,green,#671a74ff)";
-        document.getElementById("darkMode").innerText = "🔥"
+        document.getElementById("darkMode").innerText = "🌤"
     } else {
         document.body.style.backgroundImage = "linear-gradient(to right,  #671a74ff, #9c27b0,green, black)";
         document.getElementById("darkMode").innerText = "🌙"
@@ -19,7 +19,7 @@ window.onload = function () {
     dark = localStorage.getItem("darkMode") === "true"; // önce değeri al
     if (dark) {
         document.body.style.backgroundImage = "linear-gradient(to right, black, black ,green, #671a74ff)";
-        document.getElementById("darkMode").innerText = "🔥";
+        document.getElementById("darkMode").innerText = "🌤";
     } else {
         document.body.style.backgroundImage = "linear-gradient(to right,  #671a74ff,  #9c27b0,green, black)";
         document.getElementById("darkMode").innerText = "🌙";
